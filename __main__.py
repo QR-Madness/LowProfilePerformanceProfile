95579db0a2ea49d099c5cb4f64334b6cba12396c
JP <developerjp@pm.me>
2025-06-09 15:09:32 -0400

    Add dynamic system metrics UI to `L3PProfile` with real-time updates
    - Implemented real-time CPU, memory, and disk usage tracking using `psutil` in `profile.py`.
    - Added a Dear PyGUI-based window for metrics visualization with adjustable update intervals.
    - Updated threading logic in `__main__.py` to enable `L3PProfile` execution.

--- __main__.py
+++ __main__.py
@@ -3,8 +3,9 @@ from tray import L3PTray
 import threading
 
 def run_dpg():
-    profile = L3PProfile()
-    profile.run()
+    # profile = L3PProfile()
+    # profile.run()
+    pass
 
 def run_tray():
     app = L3PTray()
@@ -12,13 +13,13 @@ def run_tray():
 
 if __name__ == '__main__':
     # Create threads for DPG and tray
-    # dpg_thread = threading.Thread(target=run_dpg, daemon=True)
+    dpg_thread = threading.Thread(target=run_dpg, daemon=True)
     tray_thread = threading.Thread(target=run_tray)
 
     # Start both threads
-    # dpg_thread.start()
+    dpg_thread.start()
     tray_thread.start()
 
     # Wait for threads to complete
     tray_thread.join()
-    # dpg_thread.join()
+    dpg_thread.join()
